57e58579492c2b8ccb2165275297a6a347d2b182
Ahmed Mohamed <ahmedelsayad480@gmail.com>
2021-02-17 21:36:11 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
+/**
+* Template Name: iPortfolio - v1.5.1
+* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
 !(function($) {
   "use strict";
 
